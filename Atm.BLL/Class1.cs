cc754bd9bf342d15a1e077ce7d6ba50fe22317e2
Joshua Eze <ezejoshuac@gmail.com>
2023-02-14 09:54:08 +0100

    Add project files.

---
+++ Atm.BLL/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Atm.BLL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Atm.DAL/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Atm.DAL
+{
+    public class Class1
+    {
+
+    }
+}
